117d7561fa6720a5ae294b5c99921d92f1553984
shanonim <indigo.03sg@gmail.com>
2015-12-08 00:15:35 +0900

    [modify]照度センサの値によってslack通知の内容を分岐する

--- Photoresistor/photoresistor.js
+++ Photoresistor/photoresistor.js
@@ -16,12 +16,14 @@ var ds = milkcocoa.dataStore('brightness');
 
 // arduino
 var board = new five.Board();
-var led = new five.Led(13);
+// var led = new five.Led(13);
 var sensor;
+var isDark = false;
+var isLight = false;
 
 board.on('ready', function() {
     // LED ON
-    led.on();
+    // led.on();
     //slackに起動通知
     slackNode.api('chat.postMessage', {
         text: '電源が入ったよー！',
@@ -40,20 +42,50 @@ board.on('ready', function() {
         ds.push({'brightness' : this.value});
         console.log(this.value);
     });
-    // 照度が一定値を下回ったらLED OFF
-    sensor.within([0, 700], function() {
-        led.off();
+    // 照度が一定値を上回ったらLED ON
+    sensor.within([401, 1000], function() {
+        // led.on();
         //slackに通知
-        slackNode.api('chat.postMessage', {
-            text: '暗くなったにゃ！',
-            channel: '#iot-room',
-            as_user: true
-        }, function(err, response) {
-            console.log(response);
-        });
-    })
+        if (!isLight) {
+            slackNode.api('chat.postMessage', {
+                text: '明るいにゃ〜',
+                channel: '#iot-room',
+                as_user: true
+            }, function(err, response) {
+                console.log(response);
+            });
+            isLight = true;
+            isDark = false;
+        }
+    });
+    // 照度が一定値を下回ったらLED OFF
+    sensor.within([0, 400], function() {
+        // led.off();
+        //slackに通知
+        if (!isDark) {
+            slackNode.api('chat.postMessage', {
+                text: '暗いにゃ〜',
+                channel: '#iot-room',
+                as_user: true
+            }, function(err, response) {
+                console.log(response);
+            });
+            isDark = true;
+            isLight = false;
+        }
+    });
+});
+
+slackClient.on('open', function() {
+    console.log('open');
 });
 
 slackClient.on('message', function(message) {
     console.log(message);
 });
+
+slackClient.on('error', function(error) {
+    console.error('Error: %s', error);
+});
+
+slackClient.login();
